23e3af0aac2fed343a0630476795de113642ccba
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-09-10 15:07:51 +0530

    Everything Done

---
+++ CountDownTimer_1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace CountDownTimer_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeDisplay = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timeProgressBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeDisplay
+            // 
+            this.timeDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeDisplay.Location = new System.Drawing.Point(217, 48);
+            this.timeDisplay.Name = "timeDisplay";
+            this.timeDisplay.Size = new System.Drawing.Size(152, 38);
+            this.timeDisplay.TabIndex = 0;
+            this.timeDisplay.Text = "00:00:00";
+            this.timeDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.timeDisplay.TextChanged += new System.EventHandler(this.timeDisplay_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(94, 229);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(437, 229);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timeProgressBar
+            // 
+            this.timeProgressBar.Location = new System.Drawing.Point(94, 143);
+            this.timeProgressBar.Name = "timeProgressBar";
+            this.timeProgressBar.Size = new System.Drawing.Size(418, 30);
+            this.timeProgressBar.TabIndex = 3;
+            this.timeProgressBar.Click += new System.EventHandler(this.timeProgressBar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 318);
+            this.Controls.Add(this.timeProgressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.timeDisplay);
+            this.Name = "Form1";
+            this.Text = "One Minute Count Down Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox timeDisplay;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar timeProgressBar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CountDownTimer_1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Globalization;
+
+namespace CountDownTimer_1
+{
+    public partial class Form1 : Form
+    {
+        int cdtmr;
+        int initial_;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void timeDisplay_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void timeProgressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var timeString = timeDisplay.Text;
+            int k = (int) TimeSpan.ParseExact(timeString, "hh\\:mm\\:ss", CultureInfo.InvariantCulture).TotalSeconds;
+
+            initial_ = k;
+            cdtmr = initial_;
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+
+            timeProgressBar.Maximum = initial_;
+            btnStart.Enabled = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cdtmr > 0)
+            {
+                cdtmr--;
+                TimeSpan time = TimeSpan.FromSeconds(cdtmr);
+                timeDisplay.Text = time.ToString(@"hh\:mm\:ss");
+                timeProgressBar.Value = initial_ - cdtmr;
+            }
+            else
+            {
+
+                btnStart.Enabled = true;
+                timer1.Stop();
+
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            cdtmr = 0;
+            timer1.Stop();
+            timeDisplay.Text = "00:00:00";
+            timeProgressBar.Value = 0;
+            btnStart.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
